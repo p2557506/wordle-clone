618951e2c6bb3646d61f9a4c77fa3e16ca37a292
Isaac <admin@admins-air.home>
2023-05-27 14:55:25 +0100

    Navbar styling, letter grid styling

---
+++ frontend/src/components/Grid/Grid.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./grid.scss";
+function Grid() {
+  return (
+    <div className="grid">
+        <div className="letterBox">
+            a
+        </div>
+        <div className="letterBox">
+            a
+        </div>
+        <div className="letterBox">
+            a
+        </div>
+        <div className="letterBox">
+            a
+        </div>
+        <div className="letterBox">
+            a
+        </div>
+        <div className="letterBox">
+            a
+        </div>
+        <div className="letterBox">
+            a
+        </div>
+    </div>
+  )
+}
+
+export default Grid
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./navbar.scss"
+
+function Navbar() {
+  return (
+    <div className="navbar"><h1>Wordle</h1></div>
+  )
+}
+
+export default Navbar
